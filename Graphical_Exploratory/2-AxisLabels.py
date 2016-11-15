314bbc18b071f63c8e6d1ad4b68d8cc7ba198a41
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-11-16 00:12:43 +0530

    Axis Labels

---
+++ Graphical_Exploratory/2-AxisLabels.py
@@ -0,0 +1,15 @@
+"""
+instructions --
+
+- Label the axes. Don't forget that you should always include units in your axis labels. Your yy-axis label is just 'count'. Your xx-axis
+label is 'petal length (cm)'. The units are essential!
+- Redraw the plot constructed in the above steps using plt.draw(). Like with plt.show(), you do not need to assign this to _.
+"""
+
+# Label axes
+plt.xlabel('petal length (cm)')
+plt.ylabel('count')
+
+# Show histogram
+plt.draw()
+plt.show()
