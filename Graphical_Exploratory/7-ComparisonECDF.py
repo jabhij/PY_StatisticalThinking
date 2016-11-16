ee208df01dd4a358a0613fa69990b24570478df4
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-11-17 02:39:59 +0530

    Comparison of ECDF

---
+++ Graphical_Exploratory/7-ComparisonECDF.py
@@ -0,0 +1,30 @@
+"""
+Instructions --
+
+- Compute ECDFs for each of the three species using your ecdf() function. The variables setosa_petal_length, versicolor_petal_length, and 
+virginica_petal_length are all in your namespace. Unpack the ECDFs into x_set, y_set, x_vers, y_vers and x_virg, y_virg, respectively.
+- Plot all three ECDFs on the same plot as dots. To do this, you will need three plt.plot() commands. Assign the result of each to _.
+- Specify 2% margins.
+- Hit submit to add a legend and axis labels and to see all ECDFs!
+"""
+
+# Compute ECDFs
+x_set, y_set = ecdf(setosa_petal_length)
+x_vers, y_vers = ecdf(versicolor_petal_length)
+x_virg, y_virg = ecdf(virginica_petal_length)
+
+# Plot all ECDFs on the same plot
+_ = plt.plot(x_set, y_set, marker='.', linestyle='none')
+_ = plt.plot(x_vers, y_vers, marker='.', linestyle='none')
+_ = plt.plot(x_virg, y_virg, marker='.', linestyle='none')
+
+# Make nice margins
+plt.margins(0.02)
+
+# Annotate the plot
+plt.legend(('setosa', 'versicolor', 'virginica'), loc='lower right')
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('ECDF')
+
+# Display the plot
+plt.show()
